04dd450c777232446f0a83810fc82b5096909fe2
Aleksandr <tkdespadon@mail.ru>
2023-02-02 23:22:10 +0300

    task01

--- task01.py
+++ task01.py
@@ -1,1 +1,4 @@
-print('привет')
+username = input('Введите Ваше имя: ')
+password = input('Введите Ваш пароль: ')
+age = input ('Введите Ваш возраст: ')
+print(f'Ваше имя- {username}, Ваш пароль- {password}, Ваш возраст- {age} лет')
