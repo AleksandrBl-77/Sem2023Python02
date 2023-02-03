38e23aa8e38244974eb96650521c28684c553a5e
Aleksandr <tkdespadon@mail.ru>
2023-02-03 15:35:20 +0300

    task04-02

--- task04.py
+++ task04.py
@@ -2,10 +2,10 @@ revenue = int (input('Введите вуручку Вашей фирмы: '))
 costs = int (input('Введите издержки Вашей фирмы: '))
 worker = int (input('Введите количестко сотрудников Вашей фирмы: '))
 result = (revenue - costs)
-profitability = (revenue / costs)
+profitability = (result /revenue)
 if result > 0:
     print(f'финансовый результат - Прибыль в размаре {result} рентабельность {profitability}')
-    print(f'Прибыль фирмы в расчете на одного сотрудника = {result / profitability}')
+    print(f'Прибыль фирмы в расчете на одного сотрудника = {result / worker}')
 elif result == 0:
     print('финансовый результат - Фирма работает в 0')
 else:
