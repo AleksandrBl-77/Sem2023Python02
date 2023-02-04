1de4870d5645e58e4a51ea336d22f25bca301c03
Aleksandr <tkdespadon@mail.ru>
2023-02-04 22:11:43 +0300

    Les2-02

---
+++ L2-task03.py
@@ -0,0 +1,10 @@
+my_int = 5
+my_str = "Hello"
+my_float = 1.2
+my_bool = True
+my_dict = {'city': 'Minsk', 'country': 'Belarus'}
+
+super_list = [my_int, my_float, my_str, my_bool, my_dict]
+for i in super_list:
+    print(f'{i} тип {type(i)}')
+
